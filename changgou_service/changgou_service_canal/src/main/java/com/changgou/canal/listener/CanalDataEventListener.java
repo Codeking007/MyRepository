0c8ee18f997bde84cd18910a0990db65cf0a37bf
wfy <780118685@qq.com>
2020-01-17 13:12:18 +0800

    第三次提交,第五天开始

---
+++ changgou_service/changgou_service_canal/src/main/java/com/changgou/canal/listener/CanalDataEventListener.java
@@ -0,0 +1,118 @@
+package com.changgou.canal.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.order.pojo.Content;
+import com.xpand.starter.canal.annotation.*;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+
+@CanalEventListener
+public class CanalDataEventListener {
+    @Autowired
+    private ContentFeign contentFeign;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 新增监听
+     *
+     * @param eventType 变更操作的类型
+     * @param rowData   此次变更的数据
+     */
+    @InsertListenPoint
+    public void onEventInsert(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+//        rowData.getBeforeColumnsList():数据变更前的内容
+//        rowData.getAfterColumnsList():数据变更后的内容
+        System.out.println("--------新增监听--------");
+        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
+            System.out.println(column.getName() + ":" + column.getValue());
+        }
+    }
+
+    /**
+     * 更新监听
+     *
+     * @param eventType
+     * @param rowData
+     */
+    @UpdateListenPoint
+    public void onEventUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        System.out.println("--------更新监听--------");
+        int i = 0;
+        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
+            //获取修改前的数据
+            CanalEntry.Column beforeColumns = rowData.getBeforeColumns(i);
+            //如果修改了字段
+            if (!beforeColumns.getValue().equals(column.getValue())) {
+                System.out.println("更新了字段:" + column.getName() + "   ");
+                System.out.println(beforeColumns.getValue() + "-->" + column.getValue());
+            }
+            i++;
+        }
+    }
+
+    /**
+     * 删除监听
+     *
+     * @param eventType
+     * @param rowData
+     */
+    @DeleteListenPoint
+    public void onEventDelete(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        System.out.println("--------删除监听--------");
+        for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
+            System.out.println(column.getName() + ":" + column.getValue());
+        }
+    }
+
+    /**
+     * 自定义监听
+     * destination: 必须和canal.properties配置文件中的canal.destination属性名字相同
+     * schema: 监听的数据库
+     * table: 监听的表
+     * eventType: 监听的操作类型
+     *
+     * @param eventType
+     * @param rowData
+     */
+    @ListenPoint(destination = "example", schema = "changgou_content", table = "tb_content", eventType = {CanalEntry.EventType.INSERT, CanalEntry.EventType.UPDATE, CanalEntry.EventType.DELETE})
+    public void onEventCustomUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        System.out.println("--------广告监听--------");
+        String categoryId = "";
+        //新增
+        if (eventType == CanalEntry.EventType.INSERT) {
+            categoryId = rowData.getAfterColumns(1).getValue();
+            //修改
+        } else if (eventType == CanalEntry.EventType.UPDATE) {
+            categoryId = rowData.getAfterColumns(1).getValue();
+
+            String categoryIdBefore = rowData.getBeforeColumns(1).getValue();
+
+            //如果广告类别修改了
+            if (!categoryIdBefore.equals(categoryId)) {
+                Result<List<Content>> contents = contentFeign.findByCategoryId(Long.valueOf(categoryId));
+                if (contents.getData() != null) {
+                    //1 StringRedisTemplate.opsForValue().* //操作String字符串类型
+                    //2 StringRedisTemplate.delete(key/collection) //根据key/keys删除
+                    //3 StringRedisTemplate.opsForList().*  //操作List类型
+                    //4 StringRedisTemplate.opsForHash().*  //操作Hash类型
+                    //5 StringRedisTemplate.opsForSet().*  //操作set类型
+                    //6 StringRedisTemplate.opsForZSet().*  //操作有序set
+                    stringRedisTemplate.boundValueOps("content_" + categoryIdBefore).set(JSON.toJSONString(contents.getData()));
+                }
+            }
+            //删除
+        } else {
+            categoryId = rowData.getBeforeColumns(1).getValue();
+        }
+        Result<List<Content>> contents = contentFeign.findByCategoryId(Long.valueOf(categoryId));
+        if (contents.getData() != null) {
+            stringRedisTemplate.boundValueOps("content_" + categoryId).set(JSON.toJSONString(contents.getData()));
+        }
+    }
+}
